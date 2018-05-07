b00844cc50a9379a5eeb2db780357d0fedacbd3d
xiaoyan159-qq <xiaoyan159@qq.com>
2018-05-08 01:24:46 +0800

    测试

--- app/src/main/java/com/example/xiaoxiao/geooss_android/GeoCameraViewActivity.java
+++ app/src/main/java/com/example/xiaoxiao/geooss_android/GeoCameraViewActivity.java
@@ -189,7 +189,7 @@ public class GeoCameraViewActivity extends BaseActivity {
             @Override
             public void onVideoTaken(File video) {
                 super.onVideoTaken(video);
-
+ 
             }
         });
     }
