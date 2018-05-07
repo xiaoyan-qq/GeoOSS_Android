e729176e4cf83ba03715a61fad9c4ea99f23d442
xiaoxiao-qq <xiaoyan159@qq.com>
2018-05-07 18:32:39 +0800

    增加打开指定url功能

---
+++ app/src/main/java/com/example/xiaoxiao/geooss_android/base/FragmentKeyDown.java
@@ -0,0 +1,13 @@
+package com.example.xiaoxiao.geooss_android.base;
+
+import android.view.KeyEvent;
+
+/**
+ * Created by cenxiaozhong on 2017/5/23.
+ * source code  https://github.com/Justson/AgentWeb
+ */
+
+public interface FragmentKeyDown {
+
+    boolean onFragmentKeyDown(int keyCode, KeyEvent event);
+}
