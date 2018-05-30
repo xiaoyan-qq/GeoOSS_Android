00c221e923f26bb1c6ba16cdcd42e5f648e0e48b
xiaoxiao-qq <xiaoyan159@qq.com>
2018-05-30 17:32:45 +0800

    增加获取token的代码段

--- app/src/main/java/com/example/xiaoxiao/geooss_android/GeoCameraViewActivity.java
+++ app/src/main/java/com/example/xiaoxiao/geooss_android/GeoCameraViewActivity.java
@@ -17,9 +17,12 @@ import com.alibaba.sdk.android.oss.OSSClient;
 import com.alibaba.sdk.android.oss.ServiceException;
 import com.alibaba.sdk.android.oss.callback.OSSCompletedCallback;
 import com.alibaba.sdk.android.oss.callback.OSSProgressCallback;
+import com.alibaba.sdk.android.oss.common.OSSConstants;
 import com.alibaba.sdk.android.oss.common.OSSLog;
 import com.alibaba.sdk.android.oss.common.auth.OSSCredentialProvider;
-import com.alibaba.sdk.android.oss.common.auth.OSSStsTokenCredentialProvider;
+import com.alibaba.sdk.android.oss.common.auth.OSSFederationCredentialProvider;
+import com.alibaba.sdk.android.oss.common.auth.OSSFederationToken;
+import com.alibaba.sdk.android.oss.common.utils.IOUtils;
 import com.alibaba.sdk.android.oss.internal.OSSAsyncTask;
 import com.alibaba.sdk.android.oss.model.PutObjectRequest;
 import com.alibaba.sdk.android.oss.model.PutObjectResult;
@@ -38,7 +41,12 @@ import com.yanzhenjie.permission.Permission;
 import com.yanzhenjie.permission.Rationale;
 import com.yanzhenjie.permission.RequestExecutor;
 
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
@@ -235,31 +243,59 @@ public class GeoCameraViewActivity extends BaseActivity {
         String endpoint = "http://oss-cn-beijing.aliyuncs.com";
         // 在移动端建议使用STS方式初始化OSSClient。
         // 更多信息可查看sample 中 sts 使用方式(https://github.com/aliyun/aliyun-oss-android-sdk/tree/master/app/src/main/java/com/alibaba/sdk/android/oss/app)
-        OSSCredentialProvider credentialProvider = new OSSStsTokenCredentialProvider("LTAI8WtDDODaqxEI", "3REJHb4GcDGvSF5ZyVl4otoUYzbv8I", "acs:ram::1453306301353415:role/xiaoxiao-role");
+//        OSSCredentialProvider credentialProvider = new OSSStsTokenCredentialProvider("LTAI8WtDDODaqxEI", "3REJHb4GcDGvSF5ZyVl4otoUYzbv8I", "acs:ram::1453306301353415:role/xiaoxiao-role");
+        OSSCredentialProvider credentialProvider = new OSSFederationCredentialProvider() {
+            @Override
+            public OSSFederationToken getFederationToken() {
+                try {
+                    URL stsUrl = new URL("http://204.152.208.92:7080/distribute-token.json");
+                    HttpURLConnection conn = (HttpURLConnection) stsUrl.openConnection();
+                    InputStream input = conn.getInputStream();
+                    String jsonText = IOUtils.readStreamAsString(input, OSSConstants.DEFAULT_CHARSET_NAME);
+                    JSONObject jsonObjs = new JSONObject(jsonText);
+                    String ak = jsonObjs.getString("AccessKeyId");
+                    String sk = jsonObjs.getString("AccessKeySecret");
+                    String token = jsonObjs.getString("SecurityToken");
+                    String expiration = jsonObjs.getString("Expiration");
+                    return new OSSFederationToken(ak, sk, token, expiration);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
+
         //该配置类如果不设置，会有默认配置，具体可看该类
         ClientConfiguration conf = new ClientConfiguration();
         conf.setConnectionTimeout(15 * 1000); // 连接超时，默认15秒
         conf.setSocketTimeout(15 * 1000); // socket超时，默认15秒
         conf.setMaxConcurrentRequest(5); // 最大并发请求数，默认5个
         conf.setMaxErrorRetry(2); // 失败后最大重试次数，默认2次
-        //开启可以在控制台看到日志，并且会支持写入手机sd卡中的一份日志文件位置在SDCard_path\OSSLog\logs.csv  默认不开启
-        //日志会记录oss操作行为中的请求数据，返回数据，异常信息
-        //例如requestId,response header等
-        //android_version：5.1  android版本
-        //mobile_model：XT1085  android手机型号
-        //network_state：connected  网络状况
-        //network_type：WIFI 网络连接类型
-        //具体的操作行为信息:
-        //[2017-09-05 16:54:52] - Encounter local execpiton: //java.lang.IllegalArgumentException: The bucket name is invalid.
-        //A bucket name must:
-        //1) be comprised of lower-case characters, numbers or dash(-);
-        //2) start with lower case or numbers;
-        //3) be between 3-63 characters long.
-        //------>end of log
         OSSLog.enableLog();
         OSS oss = new OSSClient(getApplicationContext(), endpoint, credentialProvider);
         uploadData(oss);
     }
+
+    OSSCredentialProvider credetialProvider = new OSSFederationCredentialProvider() {
+        @Override
+        public OSSFederationToken getFederationToken() {
+            try {
+                URL stsUrl = new URL("http://localhost:8080/distribute-token.json");
+                HttpURLConnection conn = (HttpURLConnection) stsUrl.openConnection();
+                InputStream input = conn.getInputStream();
+                String jsonText = IOUtils.readStreamAsString(input, OSSConstants.DEFAULT_CHARSET_NAME);
+                JSONObject jsonObjs = new JSONObject(jsonText);
+                String ak = jsonObjs.getString("AccessKeyId");
+                String sk = jsonObjs.getString("AccessKeySecret");
+                String token = jsonObjs.getString("SecurityToken");
+                String expiration = jsonObjs.getString("Expiration");
+                return new OSSFederationToken(ak, sk, token, expiration);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    };
 
     /**
      * @method :
